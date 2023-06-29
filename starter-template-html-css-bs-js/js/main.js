a711ee678e3a141264012b9ccf5e5237ec52d997
Giacomotestori <giacomo.testori10@gmail.com>
2023-06-29 15:19:09 +0200

    repository completeted without bonus

--- starter-template-html-css-bs-js/js/main.js
+++ starter-template-html-css-bs-js/js/main.js
@@ -1,4 +1,15 @@
 
-let name= prompt("inserisci il tuo nome");
+let nameUser= prompt("inserisci il tuo nome");
 let surname= prompt("inserisci il tuo cognome");
 let favoritecolor= prompt("iserisci il tuo colore preferito");
+
+
+
+let passwordgenerated= nameUser+surname+favoritecolor+21
+
+
+
+console.log (passwordgenerated);
+
+
+
