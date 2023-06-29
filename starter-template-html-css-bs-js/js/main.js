27ac623b0b97ef64226c60285d222cb626a5e775
Giacomotestori <giacomo.testori10@gmail.com>
2023-06-29 15:33:57 +0200

    repository bonus generated number

--- starter-template-html-css-bs-js/js/main.js
+++ starter-template-html-css-bs-js/js/main.js
@@ -1,15 +1,22 @@
-
+/* PROMPT*/
 let nameUser= prompt("inserisci il tuo nome");
 let surname= prompt("inserisci il tuo cognome");
 let favoritecolor= prompt("iserisci il tuo colore preferito");
+let selectnumber= prompt("inserisci un numero");
+let selectsecondnumber= prompt("inserisci un secondo numero");
+
+let dividednumber = selectnumber/selectsecondnumber
 
 
 
-let passwordgenerated= nameUser+surname+favoritecolor+21
+let passwordgenerated= nameUser+surname+favoritecolor+dividednumber
 
 
 
 console.log (passwordgenerated);
+
+
+document.getElementById("Username").innerHTML = passwordgenerated;
 
 
 
